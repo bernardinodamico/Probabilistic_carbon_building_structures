b4ef06999f989c1d12c13038e794f19fa0b2f66d
Bernardino <bernardino@preoptima.com>
2023-11-11 11:15:55 +0000

    x

--- Bayesian_net/Build_ProbTables.py
+++ Bayesian_net/Build_ProbTables.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 from pandas import DataFrame 
+from tabulate import tabulate
 
 
 class Build_ProbTables():
@@ -11,54 +12,58 @@ class Build_ProbTables():
         
         return
     
-    def pr_table(self, vars_name: list[str]) -> DataFrame:
+    def pr_table(self, vars: list[str]) -> DataFrame:
         '''
         Returns the probability table of a list of variables.
-        If 'vars_name' contains only one variable -> the marginal probability table of that variable is returned, 
+        If 'vars' contains only one variable -> the marginal probability table of that variable is returned, 
         Else, the joint probability table of those variables is returned insted.
         '''
-        s = self.dataset.value_counts(vars_name, normalize=True)
-        df = s.to_frame()
+        series = self.dataset.value_counts(vars, normalize=True)
+        df_1 = series.index.to_frame().reset_index(drop=True)
+        df_2 = series.to_frame().reset_index(drop=True)
+        df_3 = df_1.join(other=df_2)
         
         st = ''
-        for name in vars_name:
+        for name in vars:
             st = st+str(name)+","
         st = st[:-1]
         
-        j_prob_table =  df.rename(columns={'proportion': 'Pr('+st+')'})
+        j_prob_table =  df_3.rename(columns={'proportion': 'Pr('+st+')'})
         
         return j_prob_table
 
 
-    def cond_pr_table(self, var: str, evidence_vars: list[str]) -> DataFrame:
+    def cond_pr_table(self, var: str, given_vars: list[str]) -> DataFrame:
         '''
         Returns the conditional probability table of one single variable "var" given a list of evidence variables.
         '''
-        s = self.dataset.value_counts([var] + evidence_vars, normalize=True) / self.dataset.value_counts(evidence_vars, normalize=True)
-        df = s.to_frame()
-        
+        series = self.dataset.value_counts([var] + given_vars, normalize=True) / self.dataset.value_counts(given_vars, normalize=True)
+        df_1 = series.index.to_frame().reset_index(drop=True)
+        df_2 = series.to_frame().reset_index(drop=True)
+        df_3 = df_1.join(other=df_2)
+
         st_ev = var+" | "
-        for name in evidence_vars:
+        for name in given_vars:
             st_ev = st_ev+str(name)+","
         st_ev = st_ev[:-1]
         
-        c_prob_table =  df.rename(columns={'proportion': 'Pr('+st_ev+')'})
+        c_prob_table = df_3.rename(columns={'proportion': 'Pr('+st_ev+')'})
         
         return c_prob_table
 
-#### Add a method to check if there are UNDEFINED probabilities in the full joint so that a warning or error is triggered 
+#### Add a method to check if there are UNDEFINED probabilities in the CPTs so that a warning or error is triggered 
 #### test these methods properly!!!
 
 probTables = Build_ProbTables()
 probTables.load_dataset(path="Bayesian_net/tests/dummy_dataset.csv")
 
-mpt = probTables.pr_table(vars_name=['Temp'])
+mpt = probTables.pr_table(vars=['Temp'])
 print(mpt)
-mpt2 = probTables.pr_table(vars_name=['Weather'])
+mpt2 = probTables.pr_table(vars=['Weather'])
 print(mpt2)
-jpt = probTables.pr_table(vars_name=['Temp', 'Weather'])
+jpt = probTables.pr_table(vars=['Temp', 'Weather'])
 print(jpt)
 
+cpt = probTables.cond_pr_table(var='Weather', given_vars=['Temp'])
 
-xxx = probTables.cond_pr_table(var='Weather', evidence_vars=['Temp'])
-print(xxx)
+print(cpt)
