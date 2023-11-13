70af3534c2d0f277c7657fa20d43df407b90a7e9
bernardinodamico <B.DAmico@napier.ac.uk>
2023-11-13 17:58:36 +0000

    x

--- Bayesian_net/Build_ProbTables.py
+++ Bayesian_net/Build_ProbTables.py
@@ -1,7 +1,8 @@
 import pandas as pd 
+import itertools
+import numpy as np
 from pandas import DataFrame 
 from tabulate import tabulate
-
 
 class Build_ProbTables():
 
@@ -12,13 +13,35 @@ class Build_ProbTables():
         
         return
     
+    def _init_pr_table(self, vars: list[str]) -> DataFrame:
+        '''
+        Initialises a probability table by entering all potential outcomes, i.e. all possible combinations of values from all variable in the "vars" list.
+        A default zero value is assigned to the probability of each outcome.
+        '''
+        vals_list = []
+        for var_name in vars:
+            vals_list.append(list(dict.fromkeys(self.dataset[var_name].to_list())))
+
+        outcomes = []
+        for outcome in itertools.product(*vals_list):
+            outcomes.append(outcome)
+
+        data = np.array([0.] * len(outcomes))
+        indexes = pd.MultiIndex.from_tuples(outcomes, names=vars)
+        _ini_series = pd.Series(data, index=indexes)
+
+        return _ini_series
+
     def pr_table(self, vars: list[str]) -> DataFrame:
         '''
         Returns the probability table of a list of variables.
         If 'vars' contains only one variable -> the marginal probability table of that variable is returned, 
         Else, the joint probability table of those variables is returned insted.
         '''
+        ini_series = self._init_pr_table(vars=vars)
         series = self.dataset.value_counts(vars, normalize=True)
+        series = ini_series.combine(other=series, func=max)
+        
         if len(vars) > 1:
             series = series.unstack(fill_value=0).stack()
         df_1 = series.index.to_frame().reset_index(drop=True)
@@ -27,8 +50,8 @@ class Build_ProbTables():
         
         st = ''
         for name in vars:
-            st = st+str(name)+","
-        st = st[:-1]
+            st = st+str(name)+", "
+        st = st[:-2]
         
         j_prob_table =  df_3.rename(columns={df_3.columns[-1]: 'Pr('+st+')'})
         
@@ -39,17 +62,20 @@ class Build_ProbTables():
         '''
         Returns the conditional probability table of one single variable "var" given a list of evidence variables.
         '''
+        ini_series = self._init_pr_table(vars=[var] + given_vars)
         series = self.dataset.value_counts([var] + given_vars, normalize=True) / self.dataset.value_counts(given_vars, normalize=True)
+        series = series.reorder_levels(order=[var] + given_vars)
+        series = ini_series.combine(other=series, func=max)
         series = series.unstack(fill_value=0).stack()
-
+        
         df_1 = series.index.to_frame().reset_index(drop=True)
         df_2 = series.to_frame().reset_index(drop=True)
         df_3 = df_1.join(other=df_2)
 
         st_ev = var+" | "
         for name in given_vars:
-            st_ev = st_ev+str(name)+","
-        st_ev = st_ev[:-1]
+            st_ev = st_ev+str(name)+", "
+        st_ev = st_ev[:-2]
         
         c_prob_table = df_3.rename(columns={df_3.columns[-1]: 'Pr('+st_ev+')'})
         
@@ -58,18 +84,19 @@ class Build_ProbTables():
 #### Now istantiations of non-existing outcomes in the dataset are accounted for in the joint and cond. tables
 # mening that zero probabilities are entered for these....Add a method to check if there are UNDEFINED probabilities in the CPTs so that a warning or error is triggered 
 #### test these methods properly!!!
-'''
-probTables = Build_ProbTables()
-probTables.load_dataset(path="Bayesian_net/tests/dummy_dataset.csv")
 
-mpt = probTables.pr_table(vars=['Temp'])
-print(mpt)
-mpt2 = probTables.pr_table(vars=['Weather'])
-print(mpt2)
-jpt = probTables.pr_table(vars=['Temp', 'Weather'])
-print(jpt)
+#probTables = Build_ProbTables()
+#probTables.load_dataset(path="Bayesian_net/tests/dummy_dataset.csv")
 
-cpt = probTables.cond_pr_table(var='Weather', given_vars=['Temp'])
+#xxx = probTables.pr_table(vars=['Weather', 'Temp', 'Wildfire'])
 
-print(cpt)
-'''
+#mpt = probTables.pr_table(vars=['Temp'])
+#print(mpt)
+#mpt2 = probTables.pr_table(vars=['Weather'])
+#print(mpt2)
+#jpt = probTables.pr_table(vars=['Temp', 'Weather'])
+#print(jpt)
+
+#cpt = probTables.cond_pr_table(var='Weather', given_vars=['Temp'])
+
+#print(cpt)
--- Bayesian_net/tests/tests.py
+++ Bayesian_net/tests/tests.py
@@ -1,20 +1,38 @@
-from unittest import TestCase 
-from Bayesian_net.Build_ProbTables import *
+import unittest
+from Bayesian_net.Build_ProbTables import Build_ProbTables
 
-
-class TestClass(TestCase):
-
+class TestBuild_ProbTables(unittest.TestCase):
 
     probTables = Build_ProbTables()
     probTables.load_dataset(path="Bayesian_net/tests/dummy_dataset.csv")
+    
+    def test_marginal_probs(self):
+        
+        mpt = self.probTables.pr_table(vars=['Temp'])
+        mpt = mpt.to_dict()
+        bench = {'Temp': {0: 3.5, 1: 10.2, 2: 26.8}, 'Pr(Temp)': {0: 0.5, 1: 0.2857142857142857, 2: 0.21428571428571427}}
 
-    mpt = probTables.pr_table(vars=['Temp'])
-    print(mpt)
-    #mpt2 = probTables.pr_table(vars=['Weather'])
-    #print(mpt2)
-    #jpt = probTables.pr_table(vars=['Temp', 'Weather'])
-    #print(jpt)
+        self.assertEqual(mpt, bench, 'marginal prob table error')
 
-    #cpt = probTables.cond_pr_table(var='Weather', given_vars=['Temp'])
+        mpt2 = self.probTables.pr_table(vars=['Weather'])
+        mpt2 = mpt2.to_dict()
+        bench2 = {'Weather': {0: 'cloudy', 1: 'rain', 2: 'sunny'}, 'Pr(Weather)': {0: 0.35714285714285715, 1: 0.35714285714285715, 2: 0.2857142857142857}}
 
-    #print(cpt)
+        self.assertEqual(mpt2, bench2, 'marginal prob table error')
+
+        mpt3 = self.probTables.pr_table(vars=['Wildfire'])
+        mpt3 = mpt3.to_dict()
+        bench3 = {'Wildfire': {0: False, 1: True}, 'Pr(Wildfire)': {0: 0.6428571428571429, 1: 0.35714285714285715}}
+
+        self.assertEqual(mpt3, bench3, 'marginal prob table error')
+
+    def test_joint_probs(self):
+        jpt = self.probTables.pr_table(vars=['Temp', 'Weather', 'Wildfire'])
+        #jpt = jpt.to_dict()
+        print(jpt)
+
+
+
+#--------------------------------------------------------------------------------
+if __name__ == '__main__':
+    unittest.main()
